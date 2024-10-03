a3f8dcf92c39638d219204c36932c5d5aa9ebbac
vinikenzo <168946875+vinikenzo@users.noreply.github.com>
2024-10-03 09:58:51 -0300

    Add files via upload

---
+++ StreamApi_1TDSPA/src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        var numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+//
+//
+//                numeros.stream().filter(numero -> numero % 2 == 0)
+//                        .map(numero -> numero * numero)
+//                        .toList().forEach(System.out::println);
+//
+
+//        var nomes = new String[]{"Ana", "Leo", "Bruno", "Cesar", "Amanda", "Antonio"};
+//
+//        var nomesComInicialA = Arrays.stream(nomes)
+//                .filter(nome -> nome.startsWith("A") && nome.length() > 3) // Filtra os nomes que comecam com a letra "A"
+//                .map(String::toUpperCase) // Transforma os nomes em letras maiúsculas
+//                .sorted() // Ordena os nomes
+//                .toList(); // Converte o Stream em uma lista
+//
+//        nomesComInicialA.forEach(System.out::println);
+
+
+//        var listas = new ArrayList<ArrayList<Integer>>();
+//        listas.add(new ArrayList<>(Arrays.asList(1,2)));
+//        listas.add(new ArrayList<>(Arrays.asList(3,4)));
+//        listas.add(new ArrayList<>(Arrays.asList(5,6)));
+//
+//        listas.stream()
+//                .flatMap(List::stream)
+//                .forEach(System.out::println);
+//
+//        var numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+//        var soma = numeros.stream()
+//                .reduce(0,Integer::sum);
+//        System.out.println();
+
+//        var palavras = Arrays.asList("java", "python", "javascript", "html");
+//        if( palavras.stream()
+//                .anyMatch(palavra -> palavra.startsWith("j"))) //anyMatch, noneMatch, allMatch
+//        System.out.println("Existe uma palavra que começa com a letra j na minha lista");
+//        else
+//        System.out.println("Existe algum elemento que não começam com a letra 'j' ");
+
+        // uma lista de 100000 numeros aleatorios
+//        var numerosAleatorios = new ArrayList<Integer>();
+//        for (int i = 0; i < 100000; i++){
+//            numerosAleatorios.add((int) (Math.random() * 1000));
+//        }
+//
+//        numerosAleatorios
+//                .parallelStream()
+//                .map(numero -> numero * 2)
+//                .forEach(System.out::println);
+
+    }
+}
